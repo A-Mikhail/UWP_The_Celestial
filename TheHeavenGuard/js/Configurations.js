87e08cc5c4ccbd1e055bf79bd3f3e7f877928df8
Mikhail Adamenko <michailadamenko@gmail.com>
2016-08-16 15:49:30 +0600

    Added dropbox links

--- TheHeavenGuard/js/Configurations.js
+++ TheHeavenGuard/js/Configurations.js
@@ -1,10 +1,20 @@
 ﻿// Google Drive documentation - https://developers.google.com/drive/v3/reference
 window.googleConfig = {
     baseUrl: "https://www.googleapis.com/drive/v3",
-    auth: "https://accounts.google.com/o/oauth2/auth?",
+    auth: "https://accounts.google.com/o/oauth2/auth",
     clientId: "678629822073-0o7dm97emlp2boen4rdei7mdpsvflvtk.apps.googleusercontent.com",
     clientSecret: "FhVndGtt4U1AuS7qFBmqbOr5",
-    redirectURI: "https://localhost/oauth2callback",
     scopes: "https://www.googleapis.com/auth/drive",
-    oauthUrl: "https://accounts.google.com/o/oauth2/token",
+    oauthUrl: "https://accounts.google.com/o/oauth2/token"
+}
+
+// Dropbox documentation - https://www.dropbox.com/developers/documentation/http/documentation
+window.dropboxConfig = {
+    rpcEndpoints: "https://api.dropboxapi.com",
+    contentUploadEndpoints: "https://content.dropboxapi.com",
+    contentDownloadEndpoints: "https://content.dropboxapi.com",
+    auth: "https://www.dropbox.com/oauth2/authorize",
+    appKey: "oyt9daobcffp5ta",
+    appSecret: "ap0blor8jkek2bu",
+    oauthUrl: "https://api.dropboxapi.com/2/auth/token/revoke"
 }

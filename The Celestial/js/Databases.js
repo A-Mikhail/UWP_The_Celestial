3f483df2e5b7197ed805f07aedd6786486a89d5f
Michael Adam <michailadamenko@gmail.com>
2016-12-13 21:03:08 +0600

    Added auto_compaction functionality for userDB; closeDB for future work, added functionality inside userDatabaseWrite with working both with files and folders; added functionality for deleting nested items if Parent folder was deleted; practice with VS comment style.

--- The Celestial/js/Databases.js
+++ The Celestial/js/Databases.js
@@ -6,8 +6,7 @@
 
     // create db for user files
     function userDB() {
-        let userDB;
-        userDB = new PouchDB("user");
+        let userDB = new PouchDB("user", { auto_compaction: true });
 
         return userDB;
     }
@@ -36,44 +35,97 @@
         });
     }
 
-    // userDatabaseWrite - function that put recieved data into UserDB 
-    // object - is files or folder
-    //
-    // dateCreated - time when object last modified
-    // name - Displayed name of object
-    // objectType - Type of object; for files - file extensions, for folder - type of folder
-    // relativeId - Unique id of objects
-    // path - Absolute path to the object
-    function userDatabaseWrite(dateCreated, name, objectType, relativeId, path) {
+    // Close database
+    // db - Database to close
+    function closeDatabase(db) {
+        Databases.db.close();
+    }
+
+    function userDatabaseWrite(dateCreated, name, objectType, relativeId, path, itemParent, nested) {
+        /// <signature>
+        /// <summary>
+        /// Write recieved parameters into UserDB();
+        /// </summary>
+        /// <param name="dateCreated" type="String">
+        /// Date when object was last modified.
+        /// </param>
+        /// <param name="name" type="String">
+        /// Displayed name of object
+        /// </param>
+        /// <param name="objectType" type="String">
+        /// Type of item; for files - file extensions.
+        /// </param>
+        /// <param name="relativeId" type="String">
+        /// Unique id of item.
+        /// </param>
+        /// <param name="path" type="String">
+        /// Absolute path to the item.
+        /// </param>
+        /// <param name="itemParent" optional="true" type="String">
+        /// Parent of item if it's nested.
+        /// </param>
+        /// <param name="nested" optional="true" type="Boolean">
+        /// </param>
+        /// </signature>
+
         // Convert name to hex for avoid error with naming
         let id = "";
 
         for (let i = 0; i < name.length; i++) {
             id += name[i].charCodeAt(0).toString(16);
         }
-        
-        Databases.userDB().put({
-            _id: id,
-            relativeId: relativeId,
-            dateCreated: dateCreated,
-            name: name,
-            objectType: objectType,
-            path: path
-        }).then(function (response) {
-            console.log("response_id: " + response.id);
-        }, function (error) {
-            messageDialog = new Windows.UI.Popups.MessageDialog(
-                "Occured error while writing in userDB"
-                + " Status: " + error.name
-                + " Message: " + error.message
-                , " Error: " + error.status);
 
-            messageDialog.showAsync();
-        });
+        // If nested equal true then add folder parent name and nested key for searching by this key
+        if (nested === true) {
+            Databases.userDB().put({
+                _id: id,
+                relativeId: relativeId,
+                dateCreated: dateCreated.toLocaleString(),
+                name: name,
+                objectType: objectType,
+                path: path,
+                itemParent: itemParent,
+                nested: "children"
+            }).catch(function (error) {
+                messageDialog = new Windows.UI.Popups.MessageDialog(
+                    "Occured error while writing items in userDB"
+                    + " Status: " + error.name
+                    + " Message: " + error.message
+                    , " Error: " + error.status);
+
+                messageDialog.showAsync();
+            });
+        } else {
+            Databases.userDB().put({
+                _id: id,
+                relativeId: relativeId,
+                dateCreated: dateCreated.toLocaleString(),
+                name: name,
+                objectType: objectType,
+                path: path,
+                nested: "root"
+            }).catch(function (error) {
+                messageDialog = new Windows.UI.Popups.MessageDialog(
+                    "Occured error while writing items in userDB"
+                    + " Status: " + error.name
+                    + " Message: " + error.message
+                    , " Error: " + error.status);
+
+                messageDialog.showAsync();
+            });
+        }
     }
 
-    // Function removeFromDatabase() - functionality for removing documents from database
     function removeFromDatabase(item) {
+        /// <signature>
+        /// <summary>
+        /// Functionality for removing items from database.
+        /// </summary>
+        /// <param name="item" type="String">
+        /// _id of removed items
+        /// </param>
+        /// </signature>
+
         // Convert name to hex for removing by using id
         let id = "";
 
@@ -81,8 +133,9 @@
             id += item[i].charCodeAt(0).toString(16);
         }
 
+        // Remove root item
         Databases.userDB().get(id).then(function (doc) {
-            return Databases.userDB().remove(doc);
+            Databases.userDB().remove(doc);
         }).catch(function (error) {
             messageDialog = new Windows.UI.Popups.MessageDialog(
                 "Occured error while removing items from userDB"
@@ -91,6 +144,37 @@
                 , " Error: " + error.status);
 
             messageDialog.showAsync();
+        });
+
+        // Remove nested item
+        // Search by name all the nested element 
+        Databases.userDB().createIndex({
+            index: { fields: ['itemParent'] }
+        }).then(function () {
+            Databases.userDB().find({
+                selector: { itemParent: { $eq: item } }
+            }).then(function (result) {
+                for (let i = 0; i < result.docs.length; i++) {
+                    Databases.userDB().remove(result.docs[i])
+                        .catch(function (error) {
+                            messageDialog = new Windows.UI.Popups.MessageDialog(
+                                "Occured error while deleting item."
+                                + " Status: " + error.name
+                                + " Message: " + error.message
+                                , " Error: " + error.status);
+
+                            messageDialog.showAsync();
+                        });
+                }
+            }).catch(function (error) {
+                messageDialog = new Windows.UI.Popups.MessageDialog(
+                    "Error. Can't find requested item for deleting."
+                    + " Status: " + error.name
+                    + " Message: " + error.message
+                    , " Error: " + error.status);
+
+                messageDialog.showAsync();
+            });
         });
     }
 

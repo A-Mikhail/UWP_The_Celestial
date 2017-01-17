acd3cb1e25cc9b44f518251c9320c4a5bfd3b389
Mikhail Adamenko <michailadamenko@gmail.com>
2017-01-17 19:43:21 +0600

    Renamed from detailedFilesManager.

---
+++ The Celestial/js/windowedFileBrowser.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+
+    let messageDialog;
+
+    WinJS.UI.Pages.define("/html/windowedFileBrowser.html", {
+        ready: function (element, options) {
+            WinJS.UI.processAll().then(function () {
+                let pivotItem = document.getElementById("pivotItem").winControl;
+
+                (window.addEventListener || window.attachEvent)(
+                    (window.attachEvent && "on" || "") + "message", function (evt) {
+                        let data = evt.data;
+
+                        // Window title
+                        Windows.UI.ViewManagement.ApplicationView.getForCurrentView().title = data;
+
+                        pivotItem.header = data;
+
+                        FileBrowser.init();
+
+                        Database.generateItems("user", "children", data);
+                    },
+                    false);
+
+                // Completely close window and release resources after the window is closed.
+                Windows.UI.ViewManagement.ApplicationView.getForCurrentView().addEventListener("consolidated", function () {
+                    window.close();
+
+                    // For Debug!
+                    if (performance && performance.mark) {
+                        performance.mark("fully ready new pivot");
+                    }
+                }, false);
+            });
+        }
+    });
+})();
